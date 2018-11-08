0fecd84ef3dfa37f7f3a6707ca51250a2b8c0394
BarstoolsDreamers <david@davidlndean.com>
2018-11-08 09:40:54 -0500

    What I completed with group

---
+++ pig.py
@@ -0,0 +1,67 @@
+import random
+
+
+class Dice:
+
+    def __init__(self):
+        pass
+
+    def roll(self):
+        dice = random.randint(1, 6)
+        print(dice)
+        return dice
+
+
+class Player:
+
+    # player needs to roll dice
+
+    # def player_score = [0]
+
+    def __init__(self, current_score, round_score, total_score, computer_player):
+        self.current_score = current_score
+        self.total_score = total_score
+        self.computer_player = computer_player
+
+    # def roll(self):
+    #     self.current_score = random.randint(1, 6)
+    #     self.total_score = self.current_score + self.total_score
+    #     print(roll)
+
+    def roll_dice():
+            input("Would you like to roll the dice? Y/N")
+        if input is input("Y"):
+            return dice
+        else
+        return ("Thanks for nothing!")
+
+        import random
+
+    def add_points(self, points):
+        self.score += points
+
+
+class Game:
+    def __init__(self, computer_player, human_player):
+        self.computer_player = computer_player
+        self.human_player = human_player
+
+    def pick_who_is_first(self):
+        return random.choice(self.computer_player, self.human_player)
+        # this function randomly chooses who rolls first
+
+    def take_turn(self, player):
+        if player == self.computer_player:
+            while player.current_score != 1:
+             # player.total_score < 21 and player.current_score != 1:
+                player.roll()
+        elif player == self.human_player:
+            choice = input("Would you like to roll?  Please type Y or N.")
+            while choice == "Y":
+                player.roll()
+                if player.current_score == 1:
+                    return
+                choice = input(
+                    "Would you like to roll again?  Pleae type Y or N.")
+            return
+
